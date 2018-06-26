6a3f0643f5de3750cda77a7501cc5ae4db9b95d2
proton9 <proton.9@live.com>
2018-06-26 16:42:33 +0530

    pre alpha working skeleton

--- README.md
+++ README.md
@@ -1,6 +1,19 @@
 Pre Alpha Skeleteon,
 pushing to work from other devices.
 
+Right now you can scrape of results from ddg,
+What this does is gives you the first result.
+
+My purpose to write this was to scrape off lyrics
+Still a pre alpha, but works.
+
+HOW to ..
+
+python lyscr.py (your search term) //could be a song, video or anything basically
+								   // you'll get the first hit, that
+								   // a lot of optimizations to be done on this
+
+
 
 ___
 prerequisites python, beautifulsoap
--- lyscr.py
+++ lyscr.py
@@ -1,8 +1,18 @@
+from bs4 import BeautifulSoup
 import sys
-import urllib2
+import requests
+from urllib.parse import unquote #take this off if it breaks 
+								 #code
+ddgfooter=''
 userInput=[]
-ddg="http://api.duckduckgo.com/?q="
-sizeOfInput=int(len(sys.argv))
+ddg="https://duckduckgo.com/html?q="
+#ddgfooter="+site%3Aazlyrics.com" use this to look
+								# for resulst in 1 particular 
+								#site
+sizeOfInput=int(len(sys.argv)) #count command line args
+
+
+
 
 print (sizeOfInput)
 if (len(sys.argv)>1):
@@ -11,10 +21,31 @@ if (len(sys.argv)>1):
 
 print(userInput)
 
-query="%20".join(map(str,userInput))
-print (query)
+#query="%20".join(map(str,userInput))
 
-print(ddg+query)
+query="+".join(userInput)
+print (query)
+url=ddg+query+ddgfooter
+print(url)
+
+page=requests.get(url)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+firstLink=soup.find_all(class_="result__a")
+#print(firstLink)
+#print(firstLink[0])
+
+print(firstLink[0]['href']) #printing the href value
+strurl = unquote(firstLink[0]['href']) #getting clean url
+noeqto=strurl.split("uddg=") #getting rid of a ddg custom tag
+print(noeqto[1])			         #clean url lies after '='
+						#that's in the 2nd element
+						#of the list
+
+#print(fistLink.prettify())
+#print(page.content)
+
+
 
 
 
